8507a8e86c17c9cb4570c0a939961d84450a7854
Keshav_Kumar <keshaviob97@gmail.com>
2024-08-03 13:02:26 +0530

    encoder lazy initialization

--- springsecuritybasic5/src/main/java/org/example/configuration/SecurityConfiguration.java
+++ springsecuritybasic5/src/main/java/org/example/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package org.example.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +19,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final DataSource dataSource;
 
     private final BCryptPasswordEncoder encoder;
+
     //in constructor injection @autowired is optional because when
     // dependencies passed through constructor spring automatically
     // understands and perform dependency injection but still if you provide
@@ -26,9 +28,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // annotation can also be used in one more case when you have multiple
     // constructor then to specify that which constructor is being used for
     // dependency injection it is helpful
+
+//    @Lazy is used with BCryptPasswordEncoder here because we are creating
+//    bean of BCryptPasswordEncoder in this SecurityConfiguration class and
+//    for same claas we are giving BCryptPasswordEncoder as a dependency in
+//    the constructor. so here it becomes a cyclic dependency and spring will
+//    not be able to resolve this on its own. so there can be 2 ways to
+//    resolve it. First approach is we mark it @Lazy so that
+//    BCryptPasswordEncoder will get lately initialized and before that
+//    SecurityConfiguration class will have time to properly initialize.
+//    Second approach,we can be to create a separate class and shift the @Bean
+//    method to that so that there is no cyclic dependency. Third approach is
+//    use field or setter injection
+
     @Autowired
-    public SecurityConfiguration(DataSource dataSource,
-                                 BCryptPasswordEncoder encoder) {
+    public SecurityConfiguration( DataSource dataSource,
+                                  @Lazy BCryptPasswordEncoder encoder) {
         this.dataSource = dataSource;
         this.encoder = encoder;
     }
